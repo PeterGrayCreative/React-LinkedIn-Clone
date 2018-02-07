b6e7c24e9a7310def8c7685122322f056b31dfc1
Unknown <peter@petergraycreative.com>
2018-02-07 15:28:56 -0600

    Create basic column structure in App.js
    Installed Bootstrap and added to index.html, deleted unneccessary files.

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <header className="header" />
+        <div class="col-md-2">
+        Left Column
+        </div>
+        <div class="col-md-7">
+        Middle Column
+        </div>
+        <div class="col-md-3">
+        Right Column
+        </div>
       </div>
     );
   }
