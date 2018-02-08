79dab7d73f6baf88f97079bea608e436cc5b4aec
Unknown <peter@petergraycreative.com>
2018-02-07 22:08:29 -0600

    Built GridItem function that gets from dummyData in UserGrid
    Added individual grid item columns with profile image, name, occupation, and connect button.
    
    Added styles for grid items, copied button style from linkedin

---
+++ linkedin/src/components/UserGrid/GridItem/GridItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './GridItem.css';
+
+function GridItem(props) {
+    return (
+    <div className="col-md-4 grid-item d-flex flex-column text-center">
+        <img src={props.user.image_url} />
+        <span className="grid-item__name">{props.user.first_name} {props.user.last_name}</span>
+        <span className="grid-item__job-title">{props.user.job_title}</span>
+        <button className="grid-item__button"><span>Connect</span></button>
+    </div>
+    )
+}
+
+export default GridItem;
