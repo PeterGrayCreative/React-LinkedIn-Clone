09601f3bf041f123de18a776065ae7a67dff760b
Unknown <peter@petergraycreative.com>
2018-02-07 21:56:40 -0600

    Add header structure
    Added header structure.
    
    Imported css, logo, and Menu component.
    
    Added header css style to Header.css which style the header container, input, and logo.

---
+++ linkedin/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Menu from "./Menu/Menu";
+import "./Header.css";
+import logo from "./logo.png";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="container header-container d-flex flex-row">
+        <div className="col-md-5 d-flex flex-row align-items-center">
+          <img className="logo" src={logo} />
+          <form>
+            <input placeholder="Search" />
+          </form>
+        </div>
+          <Menu />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
