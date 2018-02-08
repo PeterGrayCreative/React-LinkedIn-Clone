0a0b5e4bc9a1904d262938feb3178ff262552577
Unknown <peter@petergraycreative.com>
2018-02-07 21:54:00 -0600

    Add structure for main app component and styles
    Added html structure for main app component including, Header, Columns, UserGrid and smaller html parts.
    
    Imported reset.css and app.css.
    
    Imported dummy json data for grid.

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-
+import Header from "./components/Header/Header";
+import dummyData from "./dummy-data";
+import UserGrid from "./components/UserGrid/UserGrid";
+import "./reset.css";
+import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <header className="header" />
-        <div class="col-md-2">
-        Left Column
-        </div>
-        <div class="col-md-7">
-        Middle Column
-        </div>
-        <div class="col-md-3">
-        Right Column
+      <div>
+        <Header />
+        <div className="container d-flex">
+          <div className="col-md-2 column-style panel">Left Column</div>
+          <div className="col-md-7">
+            <div className="col-md-12 panel-invitation panel d-flex justify-content-between align-items-center">
+              <span>No pending Invitations</span>
+              <span class="panel-invitation__link">Manage All</span>
+            </div>
+            <UserGrid />
+          </div>
+          <div className="col-md-3 column-style panel">Right Column</div>
         </div>
       </div>
     );
