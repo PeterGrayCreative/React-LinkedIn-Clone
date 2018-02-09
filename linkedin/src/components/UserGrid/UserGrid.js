f30741a1e18460fcacf941bd9ea71657693a9118
Unknown <peter@petergraycreative.com>
2018-02-08 18:41:47 -0600

    Add filter function to generate a filtered array.
    Added filter to filter user array by search bar input data.

--- linkedin/src/components/UserGrid/UserGrid.js
+++ linkedin/src/components/UserGrid/UserGrid.js
@@ -1,18 +1,23 @@
-import React, { Component } from "react";
-import GridItem from "./GridItem/GridItem";
-import dummyData from "../../dummy-data";
+import React, { Component } from 'react';
+import GridItem from './GridItem/GridItem';
+import dummyData from '../../dummy-data';
 import './UserGrid.css';
 class UserGrid extends Component {
-  state = {
-    dummyData
-  };
+  state = { dummyData };
+
+  filteredUsers = dummyData.filter(user => {
+    const name = user.first_name + ' ' + user.last_name;
+    console.log(this.props.gridSearch);
+    return name.indexOf('g') >= 0;
+  });
+
   render() {
     return (
       <div className="user-grid d-flex flex-wrap col-md-12 panel">
-      <div className="col-md-12 user-grid__header">
-        <h3>People you may know</h3>
+        <div className="col-md-12 user-grid__header">
+          <h3>People you may know</h3>
         </div>
-        {this.state.dummyData.map(user => {
+        {this.filteredUsers.map(user => {
           return <GridItem key={user.id} user={user} />;
         })}
       </div>
