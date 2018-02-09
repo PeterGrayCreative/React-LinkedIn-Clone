a0be8265eaace1ee7c80038a0b6461e0e2821ab6
Unknown <peter@petergraycreative.com>
2018-02-08 18:38:58 -0600

    Add onChange function to receive data and event from header search child
    Added callback function for header searchbar onChange.
    
    Corrected class -> className
    
    Added state to pass to userGrid

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,22 +1,31 @@
-import React, { Component } from "react";
-import Header from "./components/Header/Header";
-import UserGrid from "./components/UserGrid/UserGrid";
-import "./reset.css";
-import "./App.css";
+import React, { Component } from 'react';
+import Header from './components/Header/Header';
+import UserGrid from './components/UserGrid/UserGrid';
+import './reset.css';
+import './App.css';
 
 class App extends Component {
+  state = {
+    searchData: '',
+  };
+  filterSearch = e => {
+    e.preventDefault();
+    this.setState({ searchData: e.target.value });
+    return;
+  };
+
   render() {
     return (
       <div>
-        <Header />
+        <Header callbackFunc={this.filterSearch} />
         <div className="container d-flex">
           <div className="col-md-2 column-style panel">Left Column</div>
           <div className="col-md-7">
             <div className="col-md-12 panel-invitation panel d-flex justify-content-between align-items-center">
               <span>No pending Invitations</span>
-              <span class="panel-invitation__link">Manage All</span>
+              <span className="panel-invitation__link">Manage All</span>
             </div>
-            <UserGrid />
+            <UserGrid gridSearchData={this.state.searchData} />
           </div>
           <div className="col-md-3 column-style panel">Right Column</div>
         </div>
