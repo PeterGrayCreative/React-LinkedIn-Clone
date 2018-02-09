d10a9b974b5a8abb4973647d77c18413733fc446
Unknown <peter@petergraycreative.com>
2018-02-08 18:39:40 -0600

    Add function to send data to parent from searchBar
    Passed props to Header and added parent function into search bar

--- linkedin/src/components/Header/Header.js
+++ linkedin/src/components/Header/Header.js
@@ -1,19 +1,19 @@
-import React from "react";
-import Menu from "./Menu/Menu";
-import "./Header.css";
-import logo from "./logo.png";
+import React from 'react';
+import Menu from './Menu/Menu';
+import './Header.css';
+import logo from './logo.png';
 
-function Header() {
+function Header(props) {
   return (
     <div className="header">
       <div className="container header-container d-flex flex-row">
         <div className="col-md-5 d-flex flex-row align-items-center">
           <img className="logo" src={logo} />
           <form>
-            <input placeholder="Search" />
+            <input placeholder="Search" onChange={props.callbackFunc} />
           </form>
         </div>
-          <Menu />
+        <Menu />
       </div>
     </div>
   );
